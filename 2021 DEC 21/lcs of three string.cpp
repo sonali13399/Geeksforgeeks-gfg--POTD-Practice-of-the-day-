0b0cb3327b781ae36b40d06e4457a45aa5417b06
SONALI PATRO <sitalsonali@gmail.com>
2021-12-21 09:57:01 +0530

    Create lcs of three string.cpp

---
+++ 2021 DEC 21/lcs of three string.cpp
@@ -0,0 +1,34 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+int LCSof3 (string A, string B, string C, int n1, int n2, int n3);
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        int n1, n2, n3; cin >> n1 >> n2 >> n3;
+        string A, B, C; cin >> A >> B >> C;
+        cout << LCSof3 (A, B, C, n1, n2, n3) << endl;
+    }
+}
+// Contributed By: Pranay Bansal
+// } Driver Code Ends
+
+
+int LCSof3 (string A, string B, string C, int n1, int n2, int n3)
+{
+    // your code here
+    int dp[n1+1][n2+1][n3+1]={};
+    for(int i=1;i<=n1;i++){
+        for(int j=1;j<=n2;j++){
+            for(int k=1;k<=n3;k++){
+                auto &ans=dp[i][j][k];
+                if(A[i-1]==B[j-1]&&B[j-1]==C[k-1])ans=max(ans,1+dp[i-1][j-1][k-1]);
+                ans=max({ans,dp[i-1][j][k],dp[i][j-1][k],dp[i][j][k-1]});
+            }
+        }
+    }
+    return dp[n1][n2][n3];
+}
