20570a983ce4ce25cc802bffc6e3fbbcb3b38736
SONALI PATRO <sitalsonali@gmail.com>
2022-02-11 15:04:07 +0530

    Create  removing leading zeros from an ip address.java

---
+++ 2022 FEB 11/removing leading zeros from an ip address.java
@@ -0,0 +1,32 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    String s = sc.next();
+                    Solution ob = new Solution();
+                    System.out.println(ob.newIPAdd(s));
+                }
+        }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public String newIPAdd(String S)
+    {
+        // your code here
+        return S.replaceAll("\\b0+(\\d+)", "$1");
+    }
+}
