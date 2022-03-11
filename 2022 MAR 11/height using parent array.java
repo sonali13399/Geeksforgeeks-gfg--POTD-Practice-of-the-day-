6e9310978010aacd097b4cc54f036c0abf74fa42
SONALI PATRO <sitalsonali@gmail.com>
2022-03-11 22:22:44 +0530

    Create height using parent array.java

---
+++ 2022 MAR 11/height using parent array.java
@@ -0,0 +1,40 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            int N = Integer.parseInt(in.readLine());
+            String a[] = in.readLine().trim().split("\\s+");
+            int arr[] = new int[N];
+            for(int i = 0;i < N;i++)
+                arr[i] = Integer.parseInt(a[i]);
+            
+            Solution ob = new Solution();
+            System.out.println(ob.findHeight(N, arr));
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int findHeight(int n, int arr[]){
+        // code here
+        int h=1,i=-1;
+        int ele=arr[n-1];
+        while(ele!=-1){
+            i=ele;
+            h++;
+            ele=arr[i];
+        }
+        return h;
+    }
+}
